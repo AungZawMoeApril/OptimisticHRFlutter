884b64844e5d49154f7059e683e80e99dbc01e9b
AZM <azmapril3@gmail.com>
2025-11-20 11:06:14 +0630

    UpdateAll

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,55 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get startDate => 'Start Date';
+
+  @override
+  String get startTime => 'Start Time';
+
+  @override
+  String get endTime => 'End Time';
+
+  @override
+  String get otHours => 'OT Hours';
+
+  @override
+  String get otType => 'OT Type';
+
+  @override
+  String get description => 'Description *';
+
+  @override
+  String get attachment => 'Attachment';
+
+  @override
+  String get attachFile => 'Attach File';
+
+  @override
+  String get pleaseSelect => 'Please select...';
+
+  @override
+  String get submit => 'Submit';
+
+  @override
+  String get ot1 => 'OT1';
+
+  @override
+  String get ot2 => 'OT2';
+
+  @override
+  String get ot3 => 'OT3';
+
+  @override
+  String get ot4 => 'OT4';
+
+  @override
+  String get overtimeRequest => 'Overtime Request';
+}
